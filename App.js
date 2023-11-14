5b3b165b893179fa9382dc63441add0147202ed8
Alberto Beltran <albertobeltran@protonmail.com>
2023-11-14 15:36:14 -0600

    initial commit - setup and login draft

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container_login}>
+        <Text style={styles.title}>Inicia sesión</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder='Expediente'
+        >
+        </TextInput>
+
+        <TextInput
+          style={styles.input}
+          placeholder='NIP'
+          secureTextEntry={true}
+        >
+        </TextInput>
+
+        <TouchableOpacity style={styles.btn}>
+          <Text style={{ fontSize: 18, color: 'white' }}>Iniciar</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +36,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container_login: {
+    display: 'flex',
+    rowGap: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '90%',
+    backgroundColor: '#F3F3F2',
+    padding: 20,
+    borderRadius: 10
+  },
+  title: {
+    marginBottom: 3,
+    fontSize: 28,
+    textAlign: 'center',
+    color: '#612165'
+  },
+  input: {
+    width: '90%',
+    backgroundColor: '#DFDEDF',
+    textAlign: 'center',
+    padding: 15,
+    fontSize: 14,
+    color: '#612165',
+    borderRadius: 17.5,
+  },
+  btn: {
+    marginTop: 5,
+    backgroundColor: '#082D73',
+    width: '90%',
+    height: 55,
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 17.5,
+  }
 });
